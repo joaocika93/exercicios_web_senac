2c5c421d3a2d49759795a5c91378e359230a129b
Joao <jmcs1993@gmail.com>
2020-09-24 22:24:24 -0300

    trabalho 01

---
+++ programacao_pra_internet_I/trabalho_01/js/index.js
@@ -0,0 +1,28 @@
+function storageForm() {
+    localStorage.setItem('nome', document.forms['dadosPessoais']['nome'].value);
+    localStorage.setItem('endereco', document.forms['dadosPessoais']['endereco'].value);
+    localStorage.setItem('cidade', document.forms['dadosPessoais']['cidade'].value);
+    localStorage.setItem('estado', document.forms['dadosPessoais']['estado'].value);
+    localStorage.setItem('usuario', document.forms['dadosPessoais']['usuario'].value);
+
+}
+
+function showData() {
+    document.getElementById('showNome').innerHTML = localStorage.getItem('nome');
+    document.getElementById('showEndereco').innerHTML = localStorage.getItem('endereco');
+    document.getElementById('showCidade').innerHTML = localStorage.getItem('cidade');
+    document.getElementById('showEstado').innerHTML = localStorage.getItem('estado');
+    document.getElementById('showUsuario').innerHTML = localStorage.getItem('usuario');
+}
+
+function verifyPassword(){
+    var password = document.forms['dadosPessoais']['password'].value;
+    var confirmPassword = document.forms['dadosPessoais']['confirm_password'].value;
+
+    if (password != confirmPassword) {
+        document.forms['dadosPessoais']['password'].setCustomValidity('As senhas devem ser iguais!');
+    } else {
+        document.forms['dadosPessoais']['password'].setCustomValidity('');
+    }
+
+}
